501318520bfbfce307e60e323f8c229fed494f20
Ege Cavusoglu <egecavusoglu99@gmail.com>
2021-06-06 15:05:47 -0700

    Removed navbar links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,20 +16,20 @@ module.exports = {
         alt: "My Site Logo",
         src: "img/logo.svg",
       },
-      items: [
-        {
-          type: "doc",
-          docId: "intro",
-          position: "left",
-          label: "Tutorial",
-        },
-        { to: "/blog", label: "Blog", position: "left" },
-        {
-          href: "https://github.com/facebook/docusaurus",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
+      // items: [
+      //   {
+      //     type: "doc",
+      //     docId: "intro",
+      //     position: "left",
+      //     label: "Tutorial",
+      //   },
+      //   { to: "/blog", label: "Blog", position: "left" },
+      //   {
+      //     href: "https://github.com/facebook/docusaurus",
+      //     label: "GitHub",
+      //     position: "right",
+      //   },
+      // ],
     },
     footer: {
       style: "dark",
